590c55a71339281aed3bd5098b94b36959a46974
Kristiyan Bonev <k.s.bonev@gmail.com>
2024-01-21 16:30:36 +0200

    Day 3 test

---
+++ Hackerrank/one_week_prep_kit/day3/test/palindrome_index.py
@@ -0,0 +1,17 @@
+def palindromeIndex(s):
+    if s == s[::-1]:
+        return -1
+
+    for i in range(len(s)):
+        new_word = s[:i] + s[i + 1:]
+        if new_word == new_word[::-1]:
+            return i
+    else:
+        return -1
+
+
+# Test Cases
+print(palindromeIndex("aaab"))
+print(palindromeIndex("baa"))
+print(palindromeIndex("aaa"))
+print(palindromeIndex("abc"))
