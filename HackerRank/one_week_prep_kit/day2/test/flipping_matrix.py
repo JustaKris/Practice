72ed64ac1b2465b83b1dade6be5aa644798516af
Kristiyan Bonev <k.s.bonev@gmail.com>
2024-04-27 14:28:15 +0300

    small adjustments to day 2 test

--- HackerRank/one_week_prep_kit/day2/test/flipping_matrix.py
+++ HackerRank/one_week_prep_kit/day2/test/flipping_matrix.py
@@ -2,12 +2,9 @@
 #
 # The function is expected to return an INTEGER.
 # The function accepts 2D_INTEGER_ARRAY matrix as parameter.
-import os
 
-
-def flippingMatrix(matrix):
+def flippingMatrix(matrix) -> int:
     n = len(matrix) // 2  # Calculate the size of the submatrix
-
     # Iterate over each quadrant
     total_sum = 0
     for i in range(n):
@@ -15,21 +12,16 @@ def flippingMatrix(matrix):
             # Select the maximum element from each quadrant and add it to the total sum
             total_sum += max(matrix[i][j], matrix[i][2 * n - 1 - j], matrix[2 * n - 1 - i][j],
                              matrix[2 * n - 1 - i][2 * n - 1 - j])
-
     return total_sum
 
 
 if __name__ == '__main__':
     q = int(input().strip())
-
     for q_itr in range(q):
         n = int(input().strip())
-
         matrix = []
-
         for _ in range(2 * n):
             matrix.append(list(map(int, input().rstrip().split())))
-
         result = flippingMatrix(matrix)
         print(str(result) + '\n')
 
